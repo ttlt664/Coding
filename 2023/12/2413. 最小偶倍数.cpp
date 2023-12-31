414f4d92ec0e6a34fe9dbf46dcc26068a02cb044
23 <syldyx2020@163.com>
2023-12-31 23:40:23 +0800

    2023 final

---
+++ 2023/12/2413. 最小偶倍数.cpp
@@ -0,0 +1,12 @@
+//
+// Created by ttlto on 2023/12/31.
+//
+//辗转相除法先得到最大公约数 然后乘积除去最大公约数
+//class Solution {
+//public:
+//    int smallestEvenMultiple(int n) {
+//        int a = 2;
+//        int g = __gcd(2,n);
+//        return a * n / g ;
+//    }
+//};
