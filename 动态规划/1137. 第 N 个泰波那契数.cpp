c914fa32b96051d8e49b72f27f90eb85ef13bbbb
23 <syldyx2020@163.com>
2024-01-01 23:28:06 +0800

    2024 begin

---
+++ 动态规划/1137. 第 N 个泰波那契数.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int T[1000];
+        T[0] = 0;
+        T[1] = 1;
+        T[2] = 1;
+        for(int i=3;i<=n;i++){
+            T[i] = T[i-2]+T[i-1]+T[i-3];
+        }
+        return T[n];
+    }
+};
